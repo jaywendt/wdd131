99ce33229de15f1e019b2ff162669210455f7921
jwendt <wen16005@byui.edu>
2025-11-08 23:39:21 -0600

    submit button

---
+++ register/register.js
@@ -0,0 +1,20 @@
+
+function participateTemplate(count) {
+    let count = 1;
+
+    newParticipants = addEventListener("click", ()=>{count++});
+
+    <section class="participant${count}">
+}
+
+function submitForm(event) {
+    event.preventDefault();
+
+}
+
+function totalFees() {
+    let feeElements = document.querySelectorAll ("id^=fee");
+    console.log(feeElements);
+}
+
+
